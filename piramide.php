90b5a489fe51d83dbe98ece35b32016e1966cced
Anouar ElGhazaoui <2080049@talnet.nl>
2020-02-03 14:48:33 +0100

    done

---
+++ piramide.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hoeveel stapels wil je hebben?" .PHP_EOL;
+$x = readline("");
+
+for($i = 0; $i < $x; $i++){
+    for($j = 0; $j <= $i; $j++){
+        echo "*";
+    }
+echo "" .PHP_EOL ;
+}
